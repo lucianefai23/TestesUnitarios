152e66410fe1d25cda4a99e6096f417ff82c73a0
lucianefai23 <143642011+lucianefai23@users.noreply.github.com>
2023-12-06 19:09:41 -0300

    Update test_service_user.py

--- tests/service/test_service_user.py
+++ tests/service/test_service_user.py
@@ -13,3 +13,72 @@ class TestServiceUser:
 
         assert wait_result == result
 
+ def test_existing_user(self):
+        name_rep = 'Luciane'
+        job_rep = 'TechLead'
+        wait_result = 'User already exists'
+        service = ServiceUser()
+
+        result = service.add_user(name=name_rep, job=job_rep)
+
+        assert wait_result == result
+
+    def test_null_user(self):
+        name_null = None
+        job_null = 'Tester'
+        wait_result = 'Invalid User'
+        service = ServiceUser()
+
+        result = service.add_user(name=name_null, job=job_null)
+
+        assert wait_result == result
+
+    def test_validate_null_job(self):
+        name_null = 'Luciane'
+        job_null = None
+        wait_result = 'Invalid User'
+        service = ServiceUser()
+
+        result = service.add_user(name=name_null, job=job_null)
+
+        assert wait_result == result
+
+    def test_validate_invalid_user(self):
+        name_inv = 20
+        job_inv = 'Tester'
+        wait_result = 'Name or job need to be string'
+        service = ServiceUser()
+
+        result = service.add_user(name=name_inv, job=job_inv)
+
+        assert wait_result == result
+
+    def test_invalid_job(self):
+        name_inv = 'Luciane'
+        job_inv = 100
+        wait_result = 'Name or job need to be string'
+        service = ServiceUser()
+
+        result = service.add_user(name=name_inv, job=job_inv)
+
+        assert wait_result == result
+
+    def test_select_user_success(self):
+        name = 'Luciane'
+        wait_result = 'Job: QA Engineer'
+        service = ServiceUser()
+
+        result = service.select_user(name=name)
+
+        assert wait_result == result
+
+    def test_update_job_success(self):
+        name = 'Luciane'
+        job = 'Tech Lead'
+        wait_result = 'Job was updated'
+        service = ServiceUser()
+
+        result = service.update_user(name=name, new_job=job)
+
+        assert wait_result == result
+
